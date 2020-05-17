28692f4aa04c7c3a793ccec43a56b35f261092ec
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-05-17 19:26:11 -0300

    Initial Project

---
+++ ApartmentsManager.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApartmentsManager.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
